8d8924edbbfff09b8006a3e3a6fb4311b8d1fdf8
justincdavis <davis.justin@mssm.org>
2024-02-22 17:29:58 -0700

    Added jetson install file for pycuda

---
+++ scripts/install_pycuda_jetson.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+sudo apt update -y
+sudo apt upgrade -y
+
+echo 'export PATH=${PATH}:/usr/local/cuda/bin' >> ~/.bashrc
+echo 'export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/usr/local/cuda/lib64' >> ~/.bashrc
+source ~/.bashrc
+sudo ln -s /usr/include/locale.h /usr/include/xlocale.h
+
+pip3 install cython
+pip3 install pycuda --user
