2d89eb811dbf0322b644275f9aa6939594544dc8
justincdavis <davis.justin@mssm.org>
2024-06-26 13:36:42 -0600

    Minor update

--- src/trtutils/_trtexec.py
+++ src/trtutils/_trtexec.py
@@ -5,9 +5,11 @@
 from __future__ import annotations
 
 import subprocess
+from functools import lru_cache
 from pathlib import Path
 
 
+@lru_cache(maxsize=1)
 def find_trtexec() -> Path:
     """
     Find an instance of the trtexec binary on the system.
@@ -23,7 +25,20 @@ def find_trtexec() -> Path:
         return basic_path
 
     # If the basic path is not present, use locate and parse
-    output = subprocess.run(["locate", "trtexec"], stdout=subprocess.PIPE, check=False)
+    try:
+        output = subprocess.run(["locate", "trtexec"], stdout=subprocess.PIPE, check=False)
+    except subprocess.CalledProcessError as e:
+        err_msg = "Error processing locate command. Ensure the locate command is installed on your system"
+        raise RuntimeError(err_msg) from e
     text_output = output.stdout.decode("utf-8")
-
-    print(text_output)
+    text_lines: list[str] = text_output.split("\n")
+    # filter out empty lines and None
+    text_lines = [line for line in text_lines if line and len(line) > 0]
+    # identify only lines which contain a trtexec binary
+    potential_lines: list[str] = []
+    for line in text_lines:
+        line_path = Path(line)
+        if line_path.stem == "trtexec":
+            potential_lines.append(line_path)
+    
+    print(potential_lines)
