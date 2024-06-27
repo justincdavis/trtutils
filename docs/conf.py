0b60319442396a0e9dcaddbe798ea5759f9bfa57
justincdavis <davis.justin@mssm.org>
2024-06-26 18:00:29 -0600

    Bump version 0.0.5 -> 0.0.6

--- docs/conf.py
+++ docs/conf.py
@@ -25,7 +25,7 @@ import trtutils
 project = "trtutils"
 copyright = "2023, Justin Davis"
 author = "Justin Davis"
-version = "0.0.5"
+version = "0.0.6"
 
 assert version == trtutils.__version__  # Make sure version is consistent
 
