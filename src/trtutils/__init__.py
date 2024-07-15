581da68cbb034d3ceed373907755f1a8c25190ab
justincdavis <davis.justin@mssm.org>
2024-07-15 14:13:45 -0600

    Bump version 0.0.6 -> 0.0.7

--- src/trtutils/__init__.py
+++ src/trtutils/__init__.py
@@ -98,7 +98,7 @@ if level is not None and level.upper() not in [
     _log.warning(f"Invalid log level: {level}. Using default log level: WARNING")
 
 __author__ = "Justin Davis"
-__version__ = "0.0.6"
+__version__ = "0.0.7"
 
 import contextlib
 
