a0e127c5bc78a278dbe694438e70f89fadf4134a
justincdavis <davis.justin@mssm.org>
2024-10-02 16:12:02 -0500

    Bump version 0.1.1 -> 0.2.0

--- docs/conf.py
+++ docs/conf.py
@@ -25,7 +25,7 @@ import trtutils
 project = "trtutils"
 copyright = "2023, Justin Davis"
 author = "Justin Davis"
-version = "0.1.1"
+version = "0.2.0"
 
 assert version == trtutils.__version__  # Make sure version is consistent
 
--- src/trtutils/__init__.py
+++ src/trtutils/__init__.py
@@ -105,7 +105,7 @@ if level is not None and level.upper() not in [
     _log.warning(f"Invalid log level: {level}. Using default log level: WARNING")
 
 __author__ = "Justin Davis"
-__version__ = "0.1.1"
+__version__ = "0.2.0"
 
 
 from . import backends, core, trtexec
