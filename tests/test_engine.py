0ce69c1062605499ae11742fd8b19d648da91ddd
justincdavis <davisjustin302@gmail.com>
2024-10-14 12:55:16 -0500

    Add more engine tests

--- tests/test_engine.py
+++ tests/test_engine.py
@@ -39,6 +39,22 @@ def test_engine_run() -> None:
     assert outputs is not None
 
 
+def test_multiple_engines_run() -> None:
+    engine_path = build_engine()
+
+    engines = [
+        trtutils.TRTEngine(engine_path, warmup=False)
+        for _ in range(4)
+    ]
+
+    outputs = [
+        engine.mock_execute() for engine in engines
+    ]
+
+    for o in outputs:
+        assert o is not None
+
+
 def test_engine_run_in_thread() -> None:
     result = [False]
 
@@ -62,3 +78,40 @@ def test_engine_run_in_thread() -> None:
     thread.join()
 
     assert result[0]
+
+
+def test_multiple_engines_run_in_threads() -> None:
+    num_engines = 4
+    result = [0] * num_engines
+    num_iters = 1_000
+
+    def run(threadid: int, result: list[int], iters: int) -> None:
+        engine_path = build_engine()
+
+        engine = trtutils.TRTEngine(
+            engine_path,
+            warmup=False,
+        )
+
+        outputs = None
+        succeses = 0
+        for _ in range(iters):
+            outputs = engine.mock_execute()
+            if outputs is not None:
+                succeses += 1
+
+        assert outputs is not None
+
+        result[threadid] = succeses
+
+    threads = [
+        Thread(target=run, args=(threadid, result, num_iters), daemon=True)
+        for threadid in range(num_engines)
+    ]
+    for thread in threads:
+        thread.start()
+    for thread in threads:
+        thread.join()
+
+    for r in result:
+        assert r == num_iters
