1163504bdfc0f2ee23f7d50e66a2e9f36f4fbd73
justincdavis <davisjustin302@gmail.com>
2024-10-23 10:59:37 -0500

    Update with new jetsontools version 0.0.3

--- src/trtutils/jetson/_benchmark.py
+++ src/trtutils/jetson/_benchmark.py
@@ -3,16 +3,19 @@
 # MIT License
 from __future__ import annotations
 
-import time
 import logging
+import time
 from dataclasses import dataclass
 from pathlib import Path
+from typing import TYPE_CHECKING
 
-import numpy as np
-from jetsontools import Tegrastats, parse_tegrastats, get_energy
+from jetsontools import Tegrastats, filter_data, get_powerdraw, parse_tegrastats
 
 from trtutils._benchmark import Metric
 from trtutils._engine import TRTEngine
+
+if TYPE_CHECKING:
+    from jetsontools._parsing import Metric as JMetric  # typing fix
 
 _log = logging.getLogger(__name__)
 
@@ -20,6 +23,7 @@ _log = logging.getLogger(__name__)
 @dataclass
 class JetsonBenchmarkResult:
     latency: Metric
+    power_draw: Metric
     energy: Metric
 
 
@@ -65,47 +69,59 @@ def benchmark_engine(
                 engine.mock_execute()
 
     # list of metrics
-    metric_names = ["latency"]
-
-    # allocate spot for raw data
+    metric_names = ["latency", "power_draw", "energy"]
     raw: dict[str, list[float]] = {metric: [] for metric in metric_names}
 
     # pre-generate the false data
     false_data = engine.get_random_input()
 
     # create temp file location for data to go
-    temp_file = Path("/temp/tegra.txt")
+    temp_file = Path(Path.cwd()) / "temptegra.txt"
     # store the start/stop times of the engine execution
-    start_stop: list[tuple[float, float]] = []
+    start_stop_times: list[tuple[float, float]] = []
     with Tegrastats(temp_file, interval=tegra_interval):
         for _ in range(iterations):
             t0 = time.time()
             engine.mock_execute(false_data)
             t1 = time.time()
             raw["latency"].append(t1 - t0)
-            start_stop.append((t0, t1))
+            start_stop_times.append((t0, t1))
 
     # parse the tegra data
     tegradata = parse_tegrastats(temp_file)
 
-    # filter out all entries which are not during execution
-    # TODO: implement
-    filtered_data = tegradata
+    # delete the temp file
+    temp_file.unlink()
+
+    # filter the data by actual times during execution
+    filtered_data, per_inference = filter_data(tegradata, start_stop_times)
 
     # get the energy values
-    energy_data = get_energy(filtered_data)
+    powerdraw_data: dict[str, JMetric] = get_powerdraw(filtered_data)
+    raw["power_draw"] = powerdraw_data["VDD_TOTAL"].raw
+
+    # compute energy values
+    # for energy values need to compute powerdraw per infernece
+    # then compute energy
+    energy_data = [
+        get_powerdraw(inf_data)["VDD_TOTAL"].mean * (inf_stop - inf_start)
+        for (inf_start, inf_stop), inf_data in per_inference
+        if len(inf_data) > 0
+    ]
+    raw["energy"] = energy_data
 
     # calculate the metrics
     metrics: dict[str, Metric] = {}
-    for metric in metric_names:
-        data = np.array(raw[metric])
-        avg = float(np.mean(data))
-        min_ = float(np.min(data))
-        max_ = float(np.max(data))
-
-        _log.info(f"{metric}: avg={avg:.6f}, min={min_:.6f}, max={max_:.6f}")
-        metrics[metric] = Metric(avg, min_, max_)
+    for metric_name in metric_names:
+        data = raw[metric_name]
+        metric = Metric(data)
+        metrics[metric_name] = metric
+        _log.info(
+            f"{metric}: mean={metric.mean:.6f}, median={metric.median:.6f}, min={metric.min:.6f}, max={metric.max:.6f}",
+        )
 
     return JetsonBenchmarkResult(
         latency=metrics["latency"],
+        power_draw=metrics["power_draw"],
+        energy=metrics["energy"],
     )
