1f164cb983d332ca8f8a401a3e2871ac560c3b62
justincdavis <davisjustin302@gmail.com>
2024-10-23 11:01:46 -0500

    revert version update

--- docs/conf.py
+++ docs/conf.py
@@ -25,7 +25,7 @@ import trtutils
 project = "trtutils"
 copyright = "2023, Justin Davis"
 author = "Justin Davis"
-version = "0.2.4"
+version = "0.2.3"
 
 assert version == trtutils.__version__  # Make sure version is consistent
 
