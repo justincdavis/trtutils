57cba4fd7e6f981d180d38e172aca1803123a356
justincdavis <davisjustin302@gmail.com>
2025-02-27 17:36:47 -0600

    Add basic run cli, placeholder setup for now

---
+++ src/trtutils/impls/yolo/__main__.py
@@ -0,0 +1,10 @@
+# Copyright (c) 2024 Justin Davis (davisjustin302@gmail.com)
+#
+# MIT License
+from __future__ import annotations
+
+from ._run import run_cli
+
+
+if __name__ == "__main__":
+    run_cli()
