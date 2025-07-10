b4d97c5627cc6670f7b4766bb32fe3affaa3af91
justincdavis <davis.justin@mssm.org>
2025-07-10 08:42:44 -0500

    Add research submodule, not linked to global import yet. axonn for now

---
+++ src/trtutils/research/__init__.py
@@ -0,0 +1,6 @@
+# Copyright (c) 2025 Justin Davis (davisjustin302@gmail.com)
+#
+# MIT License
+"""Implementations for research papers."""
+
+from __future__ import annotations
---
+++ src/trtutils/research/axonn/__init__.py
@@ -0,0 +1,6 @@
+# Copyright (c) 2025 Justin Davis (davisjustin302@gmail.com)
+#
+# MIT License
+"""Implementation for the research paper AxoNN."""
+
+from __future__ import annotations
