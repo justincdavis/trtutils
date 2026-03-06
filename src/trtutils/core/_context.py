a4c779b2d7755c142cf4abf0338c3c527fb1b9f7
justincdavis <davis.justin@mssm.org>
2026-03-06 17:54:36 -0600

    fix: bug with cuda_call on CuctxCreateParams, no error code to check

--- src/trtutils/core/_context.py
+++ src/trtutils/core/_context.py
@@ -27,7 +27,7 @@ def create_context(device: int = 0) -> cuda.CUcontext:
     """
     cu_device = cuda_call(cuda.cuDeviceGet(device))
     if FLAGS.CUDA_13:
-        return cuda_call(cuda.cuCtxCreate(cuda_call(cuda.CUctxCreateParams()), 0, cu_device))
+        return cuda_call(cuda.cuCtxCreate(cuda.CUctxCreateParams(), 0, cu_device))
     return cuda_call(cuda.cuCtxCreate(0, cu_device))
 
 
