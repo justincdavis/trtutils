d1d120ef11d5f1489340f30255235f92207b2bf0
justincdavis <davisjustin302@gmail.com>
2026-03-20 20:23:08 -0500

    chore: reorder imports in init file

--- src/trtutils/__init__.py
+++ src/trtutils/__init__.py
@@ -121,9 +121,11 @@ __version__ = "0.7.0"
 
 import contextlib
 
-from . import builder, compat, core, download, image, inspect, models, parallel, research, trtexec
+from . import core
 
 FLAGS.init_device_flags()
+
+from . import builder, compat, download, image, inspect, models, parallel, research, trtexec
 from ._benchmark import BenchmarkResult, Metric, benchmark_engine, benchmark_engines
 from ._engine import TRTEngine
 from ._profile import profile_engine
